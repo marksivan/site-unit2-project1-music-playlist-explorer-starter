747632cbbad00ddd5d8b414edc0683b408c95e39
Mark Sivan Tamakloe <marktamakloe@meta.com>
2025-06-05 16:36:22 -0700

    in progress

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,15 @@
+document.querySelectorAll('.playlist-card').forEach((card) => {
+   card.addEventListener('click', function(){
+      document.querySelector('.modal-overlay').style.display = 'block';
+   });
+});
 
+document.querySelector('.close').addEventListener('click', function() {
+   document.querySelector('.modal-overlay').style.display = 'none';
+});
+
+window.addEventListener('click', function(event) {
+   if (event.target.classList.contains('modal-overlay')) {
+      document.querySelector('.modal-overlay').style.display = 'none';
+   }
+});
